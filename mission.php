b43ab881ff9bf69415cda4db83e83d5a92ba8331
ringo <M2520055@ngo.ohara.ac.jp>
2026-09-18 14:20:51 +0900

    DISARMEDに変更

--- mission.php
+++ mission.php
@@ -13,7 +13,7 @@ function overrideSecurity(): void
     ];
     // ==========================================
 
-    $fw = $system_config['firewall'] ?? null;
+    $fw = $system_config['DISARMED'] ?? null;
     $door = $system_config['vault_door'] ?? null;
 
     if ($fw === 'DISARMED' && $door === 'UNLOCKED') {
